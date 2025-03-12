2a67dea30400c87c6b46761eeb09f7a700e7e814
Jason Alanya <jasonalanya853@gmail.com>
2025-03-11 21:32:44 -0500

    Setting default API_URL to http://localhost:5000

--- frontend/js/config/config.js
+++ frontend/js/config/config.js
@@ -1,4 +1,4 @@
-export const API_URL = 'http://20.253.27.22:5000'; //change this for production
+export const API_URL = 'http://127.0.0.1:5000'; //change this for production
 
 export const uploadButtons = {
     departments: document.getElementById('upload-btn-departments'),
